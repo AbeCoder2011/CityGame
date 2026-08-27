6e09edd2d44d5d953cc5a80af3e7a3f2a5fd26cd
Coderspark <luke@slaughter.com>
2026-08-27 15:41:57 +0200

    fixed place in unlocked land

--- Code/builder.gd
+++ Code/builder.gd
@@ -38,12 +38,13 @@ func GetBuildingSize(building_name: String) -> Vector2i:
 
 func IsColliding(pos: Vector2i, size: Vector2i) -> bool:
 	var new_rect = Rect2i(pos, size)
+	print($"..".BuildableAreas)
 	for b in $"../Buildings".Buildings:
 		var b_size = GetBuildingSize(b["name"])
 		var b_rect = Rect2i(b["pos"], b_size)
 		if new_rect.intersects(b_rect):
 			return true
 	for r in $"..".BuildableAreas:
-		if new_rect.intersects(r):
+		if r.encloses(new_rect):
 			return false
 	return true
